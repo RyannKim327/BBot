3a2663511c2b6c9823933023405315697a742f33
RySes <rksesgundo123@gmail.com>
2022-03-22 21:01:24 +0800

    Add files via upload

--- script/qr.js
+++ script/qr.js
@@ -1,27 +1,25 @@
 const fs = require("fs")
-const http = require("https")
+const request = require("request")
 
 module.exports = (api, body, event) => {
 	let xpl = body.split(" ")
-	api.sendMessage("h", event.threadID)
 	xpl.shift()
 	let data = "http://api.qrserver.com/v1/create-qr-code/?150x150&data=" + xpl.join(" ")
 	let f = fs.createWriteStream("qr.jpg")
-	let res = http.get(data, (r) => {
-		r.pipe(f)
-		f.on("finish", () => {
-			api.sendMessage({
-				body: "QR Code Generated",
-				attachment: fs.createReadStream(__dirname + "/../qr.jpg").on("end", async () => {
-					if(fs.existsSync(__dirname + "/../qr.jpg")){
-						fs.unlink(__dirname + "/../qr.jpg", (err) => {
-							if(err){
-								console.log(err)
-							}
-						})
-					}
-				})
-			}, event.threadID, event.messageID)
-		})
+	let res = request(encodeURI(data))
+	res.pipe(f)
+	f.on("close", () => {
+		api.sendMessage({
+			body: "QR Code Generated",
+			attachment: fs.createReadStream(__dirname + "/../qr.jpg").on("end", async () => {
+				if(fs.existsSync(__dirname + "/../qr.jpg")){
+					fs.unlink(__dirname + "/../qr.jpg", (err) => {
+						if(err){
+							console.log(err)
+						}
+					})
+				}
+			})
+		}, event.threadID, event.messageID)
 	})
 }
