bb4fe99d5b93bf4708bb7a7b8a96d05e8e033fc3
RySes <rksesgundo123@gmail.com>
2022-03-22 20:25:26 +0800

    Add files via upload

---
+++ script/qr.js
@@ -0,0 +1,26 @@
+const fs = require("fs")
+const http = require("https")
+
+module.exports = async (api, body, event) => {
+	let xpl = body.split(" ")
+	xpl.shift()
+	let data = "http://api.qrserver.com/v1/create-qr-code/?150x150&data=" + xpl.join(" ")
+	let f = fs.createWriteStream("qr.jpg")
+	await http.get(data, (r) => {
+		r.pipe(f)
+		f.on("finish", () => {
+			api.sendMessage({
+				body: "QR Code Generated",
+				attachment: fs.createReadStream(__dirname + "/../qr.jpg").on("end", async () => {
+					if(fs.existsSync(__dirname + "/../qr.jpg")){
+						fs.unlink(__dirname + "/../qr.jpg", (err) => {
+							if(err){
+								console.log(err)
+							}
+						})
+					}
+				}
+			}, event.threadID, event.messageID)
+		})
+	})
+}
