c761fa0244a115dbec50ae9dca2af4bb65b6d264
RySes <rksesgundo123@gmail.com>
2022-03-23 08:20:51 +0800

    Add files via upload

--- script/qr.js
+++ script/qr.js
@@ -2,24 +2,46 @@ const fs = require("fs")
 const request = require("request")
 
 module.exports = (api, body, event) => {
-	let xpl = body.split(" ")
-	xpl.shift()
-	let data = "http://api.qrserver.com/v1/create-qr-code/?150x150&data=" + xpl.join(" ")
-	let f = fs.createWriteStream("qr.jpg")
-	let res = request(encodeURI(data))
-	res.pipe(f)
-	f.on("close", () => {
-		api.sendMessage({
-			body: "QR Code Generated",
-			attachment: fs.createReadStream(__dirname + "/../qr.jpg").on("end", async () => {
-				if(fs.existsSync(__dirname + "/../qr.jpg")){
-					fs.unlink(__dirname + "/../qr.jpg", (err) => {
-						if(err){
-							console.log(err)
-						}
-					})
-				}
-			})
-		}, event.threadID, event.messageID)
-	})
+	if(event.type == "message_reply"){
+		let body = event.messageReply.body
+		let data = "http://api.qrserver.com/v1/create-qr-code/?150x150&data=" + body
+		let f = fs.createWriteStream("qr.jpg")
+		let res = request(encodeURI(data))
+		res pipe(f)
+		f.on("close", () => {
+			api.sendMessage({
+				body: "QR Code Generated",
+				attachment: fs.createReadStream(__dirname + "/../qr.jpg").on("end", async () => {
+					if(fs.existsSync(__dirname + "/../qr.jpg")){
+						fs.unlink(__dirname + "/../qr.jpg", (err) => {
+							if(err){
+								console.log(err)
+							}
+						})
+					}
+				})
+			}, event.threadID, event.messageID)
+		})
+	}else{
+		let xpl = body.split(" ")
+		xpl.shift()
+		let data = "http://api.qrserver.com/v1/create-qr-code/?150x150&data=" + xpl.join(" ")
+		let f = fs.createWriteStream("qr.jpg")
+		let res = request(encodeURI(data))
+		res.pipe(f)
+		f.on("close", () => {
+			api.sendMessage({
+				body: "QR Code Generated",
+				attachment: fs.createReadStream(__dirname + "/../qr.jpg").on("end", async () => {
+					if(fs.existsSync(__dirname + "/../qr.jpg")){
+						fs.unlink(__dirname + "/../qr.jpg", (err) => {
+							if(err){
+								console.log(err)
+							}
+						})
+					}
+				})
+			}, event.threadID, event.messageID)
+		})
+	}
 }
