07cb5f966bd99cf703514f98982614f8419a06cd
RySes <rksesgundo123@gmail.com>
2022-03-25 02:51:23 +0800

    Add files via upload

--- script/wiki.js
+++ script/wiki.js
@@ -1,7 +1,6 @@
 const fws = require("fs")
 const axios = require("axios")
 const http = require("https")
-const request = require("request")
 
 async function getWiki(q) {
 	let out = await axios.get("https://en.wikipedia.org/api/rest_v1/page/summary/" + q).then((response) => {
@@ -29,45 +28,31 @@ module.exports = async (api, body, event) => {
 		w += "You've searched about " + r.title + "\n\nDescription: " + r.description + "\n\n\t" + r.extract + "\n\nSource:\nDesktop: " + r.content_urls.desktop.page + "\nMobile: " + r.content_urls.mobile.page
 		if(r.originalimage !== undefined){
 			let f = fs.createWriteStream("wiki.png")
-			await http.get(r.originalimage.source, (p) => {
-				p.pipe(f)
+			let g_r = http.get(r.originalimage.source, (r_s) => {
+				r_s.pipe(f)
 				f.on("finish", () => {
-					api.sendMessage(w, threadID, messageID)
-					api.sendMessage({
-						attachment: fs.createReadStream(__dirname + "/../wiki.png").on("end", async () => {
-							if(fs.existsSync(__dirname + "/../wiki.png")){
-								fs.unlink(__dirname + "/../wiki.png", (err) => {
-									if(err) return console.error("Error [Wiki Delete]: " + err)
-								})
-							}
-						})
-					}, threadID)
+					try{
+						console.log(f)
+						api.sendMessage({
+							body: "",
+							attachment: fs.createReadStream(__dirname + "/wiki.png").on("end", async () => {
+								if(fs.existsSync(__dirname + "/wiki.png")){
+									fs.unlink(__dirname + "/wiki.png", (err) => {
+										if(err){
+											console.log("error " + err)
+										}else{
+											console.log("Done")
+										}
+									})
+									api.sendMessage(w, event.threadID, event.messageID)
+								}
+							})
+						}, event.threadID, event.messageID)
+					}catch (err){
+						api.sendMessage(w, event.threadID)
+					}
 				})
 			})
-			/*let g_r = request(encodeURI(r.originalimage.source))
-			g_r.pipe(f)
-			f.on("close", () => {
-				try{
-					console.log(f)
-					api.sendMessage({
-						body: "Image",
-						attachment: fs.createReadStream(__dirname + "/../wiki.png").on("end", async () => {
-							if(fs.existsSync(__dirname + "/../wiki.png")){
-								fs.unlink(__dirname + "/../wiki.png", (err) => {
-									if(err){
-										console.log("error " + err)
-									}else{
-										console.log("Done")
-									}
-								})
-								api.sendMessage(w, event.threadID, event.messageID)
-							}
-						})
-					}, event.threadID, event.messageID)
-				}catch (err){
-					api.sendMessage(w, event.threadID)
-				}
-			})*/
 		}else{
 			api.sendMessage(w, event.threadID, event.messageID)
 		}
