63a556b23d7f642f92f15bcc2a7e3ff3388fdb67
RySes <rksesgundo123@gmail.com>
2022-03-24 19:09:31 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { keep_alive } = require("./keep_alive")
 const login = require("fca-unofficial")
 const fs = require("fs")
 
+const prefix = ""
+const adminPrefix = "<< "
+const adminPostfix = " >>"
+
 const gc = process.env['gc']
 let vip = []
 let gc_admin = []
@@ -24,14 +28,45 @@ login({appState: JSON.parse(process.env['state'])}, (err, api) => {
 	api.listen(async (err, event) => {
 		if(err) return console.error("Error [Listen events]: " + err)
 		if(event.body != null){
+			api.setMarkAsReadAll((err) => {
+				if(err) return console.error("Error [Mark as Read All]: " + err)
+			})
 			let threadID = event.threadID
 			let senderID = event.senderID
 			let messageID = event.messageID
 			let body = event.body
 			let low_body = body.toLowerCase()
-			if(threadID == gc){
-				api.sendMessage(low_body, gc)
+			api.getThreadInfo(threadID, (err, data) => {
+				if(err) return console.error("Error [Thread admin gc]: " + err)
+				let list = data.adminIDs
+				for(let i = 0; i < list.length; i++){
+					gc_admin.push(list[i].id)
+				}
+			})
+			if(low_body.startsWith(adminPrefix) && low_body.endsWith(adminPostfix)){
+				let command = low_body.replace(adminPrefix, "").replace(adminPostfix, "")
+				if(command == "sleep" && gc_admin.includes(senderID) && !ban_thread.includes(threadID) && !gc.includes(threadID)){
+					api.getThreadInfo(threadID, (err, data) => {
+						if(err) return console.error("Error [Admin Off]: " + err)
+						ban_thread += threadID + "/"
+						api.sendMessage("Be right back guys. Please ask my VIPs to activate me to this thread", threadID)
+						api.sendMessage(`An admin turned off the bot service on ${data.threadName}.`, gc)
+					})
+				}else if(vip.includes(senderID)){
+					if(command == "toggle"){
+						service = !service
+						if(!gc.includes(threadID)){
+							api.sendMessage("Bot service turned " ((service) ? "on" : "off") + " to all threads.", threadID)
+						}
+						api.sendMessage("Bot service turned " + ((service) ? "on" : "off") + " to all threads.", gc)
+					}
+				}
 			}
 		}
 	})
 })
+
+
+
+
+
