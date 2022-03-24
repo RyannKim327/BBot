4bfe150c41bd01b8cc5fa7fad5b4f069b20c7730
RySes <rksesgundo123@gmail.com>
2022-03-25 03:26:19 +0800

    Add files via upload

--- script/wiki.js
+++ script/wiki.js
@@ -39,6 +39,8 @@ module.exports = async (api, body, event) => {
 			})
 		}
 		api.sendMessage(w, event.threadID, event.messageID)
+	}catch(e){
+		api.sendMessage(e, event.threadID, event.messageID)
 	}
 }
 /*
