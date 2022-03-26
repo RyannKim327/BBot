7abe1a77f6cc94df7c6dd1761f5a4b642e2b4f06
RySes <rksesgundo123@gmail.com>
2022-03-26 18:19:06 +0800

    Add files via upload

--- script/sim.js
+++ script/sim.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
 
 async function s(msg){
-	let result = await axios.get("https://api-sv2.simsimi.net/v2/?text=" + msg + "&lc=en&cf=false&name=BhieBot").then((r) => {
+	let result = await axios.get("https://api-sv2.simsimi.net/v2/?text=" + msg + "&lc=ph&cf=false&name=BhieBot").then((r) => {
 		return r.data
 	}).catch((e) => {
 		console.error("Error [Simsimi]: " + e)
@@ -18,7 +18,7 @@ module.exports = (api, body, event) => {
 	data.shift()
 	s(data.join(" ")).then((r) => {
 		if(r == null){
-			api.sendMessage("Sorry, there's just having a problem on my mind.", event.threadID, event.messageID)
+			api.sendMessage("Pasensya na, nabobobo ko hahaha.", event.threadID, event.messageID)
 		}else{
 			api.sendMessage(r.success, event.threadID, event.messageID)
 		}
