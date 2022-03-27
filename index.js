be14d7371e3569f2bfd55910014745ed4807729d
RySes <rksesgundo123@gmail.com>
2022-03-28 04:07:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ login({appState: JSON.parse(process.env['state'])}, async (err, api) => {
 		})
 	})
 	let ne = await getTopNews()
-	console.log("Log [News test]: " + ne[Math.floor(Math.random() * ne.length)].headline_stores)
+	console.log("Log [News test]: " + ne[Math.floor(Math.random() * ne.length)].headline_stories)
 	api.setOptions({
 		listenEvents: true,
 		selfListen: true
