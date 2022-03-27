94a64570c32ca39ba5e0cb176d439561d4a2f1f8
RySes <rksesgundo123@gmail.com>
2022-03-28 03:58:50 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { keep_alive } = require("./keep_alive")
 const login = require("fca-unofficial")
 const fs = require("fs")
 const cron = require("node-cron")
-const news = require("googlethis")
+const { news } = require("googlethis")
 
 const date = require('./script/date')
 const filter = require("./script/filter")
@@ -75,7 +75,7 @@ login({appState: JSON.parse(process.env['state'])}, (err, api) => {
 			}
 		})
 	})
-	let ne = await news.getTopNews()
+	let ne = await news()
 	console.log("Log [News test]: " + ne)
 	api.setOptions({
 		listenEvents: true,
