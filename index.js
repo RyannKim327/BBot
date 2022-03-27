bf9a089966560b23ae0f4104983e866f70a39add
RySes <rksesgundo123@gmail.com>
2022-03-28 04:02:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { keep_alive } = require("./keep_alive")
 const login = require("fca-unofficial")
 const fs = require("fs")
 const cron = require("node-cron")
-const { news } = require("googlethis")
+const { getTopNews } = require("googlethis")
 
 const date = require('./script/date')
 const filter = require("./script/filter")
@@ -75,7 +75,7 @@ login({appState: JSON.parse(process.env['state'])}, async (err, api) => {
 			}
 		})
 	})
-	let ne = await news()
+	let ne = await getTopNews()
 	console.log("Log [News test]: " + ne)
 	api.setOptions({
 		listenEvents: true,
