a26cfada792ba3cd9b53e88d8e1633d706a6ca37
RyannKim327 <rksesgundo123@gmail.com>
2022-04-05 12:15:43 +0800

    Commitment

--- script/filter.js
+++ script/filter.js
@@ -1,33 +1,4 @@
-function f(p) {
-	let g = [
-		"bobo",
-		"bold",
-		"tanga",
-		"gaga",
-		"gago",
-		"gagu",
-		"kulangot",
-		"kwak",
-		"ulol",
-		"olol",
-		"ulul",
-		"olul",
-		"taena",
-		"tangina",
-	]
-	let w = p.split(" ")
-	for(let i = 0; i < g.length; i++){
-		for(let j = 0; j < w.length; j++){
-			if(w[j] == g[i]){
-				return true
-				break
-			}
-		}
-	}
-	return false
-	//return /^([bobo|bobu|bubo|gaga|gagi|gago|gagu|kulangot|kwak|olol|olul|ulol|ulul|potaena|shit|putanginamo|potaenamo|putang\sina|putangina|pota|tanga|taena|tangina|yawa]+)/.test(p)
-}
-
 module.exports = (body) => {
-	return f(body)
+	const regex = /(bobo|bobu|bold|bubo|bubu|fuck|fuck\syou|gaga|gago|gagu|kulangot|kwak|olol|olul|pota|potaena|putangina|putang\sina|ulol|ulul|tanga|taena|tangina|yawa)/
+	return regex.test(body)
 }
