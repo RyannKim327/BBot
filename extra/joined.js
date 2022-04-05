e35d6f9901b8ccf40c52139887ccdc5a9c5d3923
RyannKim327 <rksesgundo123@gmail.com>
2022-04-05 18:11:30 +0800

    Commitment again

--- extra/joined.js
+++ extra/joined.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 
-module.exports = async (api, event) => {
+module.exports = (api, event) => {
 	console.log("Test")
 	if(event.type == "event"){
 		console.log("Working event")
--- script/filter.js
+++ script/filter.js
@@ -1,4 +1,13 @@
 module.exports = (body) => {
-	const regex = /(bobo|bobu|bold|bubo|bubu|fuck|fuck\syou|gaga|gago|gagu|kulangot|kwak|olol|olul|pota|potaena|putangina|putang\sina|ulol|ulul|tanga|taena|tangina|yawa)/
-	return regex.test(body)
+	const regex = /^(bobo|bobu|bold|bubo|bubu|fuck|fuck\syou|gaga|gago|gagu|kulangot|kwak|olol|olul|pota|potaena|putangina|putang\sina|ulol|ulul|tanga|taena|tangina|yawa)$/i
+
+	let text = body.split(" ")
+	let res = false
+	for(let i = 0; i < text.length; i++){
+		if(regex.test(text[i])){
+			res = true
+			break
+		}
+	}
+	return res
 }
