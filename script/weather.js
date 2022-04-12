5ba96ef6fd81d6bbeee4659a2368cd1e93c6ee09
RySes <rksesgundo123@gmail.com>
2022-04-12 22:58:05 +0800

    Add files via upload

--- script/weather.js
+++ script/weather.js
@@ -1,21 +1,5 @@
 const weatherjs = require("weather-js")
 
-async function func(city){
-	let result;
-	weatherjs.find({
-		search: city,
-		degreeType: 'C'
-	}, (err, r) => {
-		if(err){
-			console.log("Error [Weather]: " + err)
-			result = null
-		}else{
-			result = r
-		}
-	})
-	return result
-}
-
 module.exports = async (api, body, event) => {
 	let data = body.split(" ")
 	if(data.length <= 2){
@@ -23,12 +7,16 @@ module.exports = async (api, body, event) => {
 	}else{
 		data.shift()
 		data.shift()
-		let r = await func(data.join(" "))
-		let d = r[0]
-		let m = "Location: " + d.location.name + "\n"
-		m += "Temperature: " + d.current.temperature + "\n"
-		m += "Sky: " + d.current.skytext + "\n"
-		m += "Observation time: " + d.current.date + " " + d.current.observationtime
-		api.sendMessage(m, event.threadID, event.messageID)
+		weatherjs.find({
+			search: data.join(" "),
+			degreeType: 'C'
+		}, (err, r) => {
+			let d = r[0]
+			let m = "Location: " + d.location.name + "\n"
+			m += "Temperature: " + d.current.temperature + "\n"
+			m += "Sky: " + d.current.skytext + "\n"
+			m += "Observation time: " + d.current.date + " " + d.current.observationtime
+			api.sendMessage(m, event.threadID, event.messageID)
+		})
 	}
 }
