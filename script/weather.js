74dd24f9edc37509c4cd9c60b55f0cc510afe06b
RySes <rksesgundo123@gmail.com>
2022-04-12 20:16:29 +0800

    Add files via upload

---
+++ script/weather.js
@@ -0,0 +1,28 @@
+const weatherjs = require("weather-js")
+
+async function func(city){
+	let result = weatherjs.find{
+		search: city,
+		degreeType: "C"
+	}, (err, r) => {
+		return r
+	}
+	return result
+}
+
+module.exports = (api, body, event) => {
+	let data = split(" ")
+	if(data.length <= 2){
+		api.sendMessage("This feature requires the city of area you want to know", event.threadID, event.messageID)
+	}else{
+		data.shift()
+		data.shift()
+		let r = func(data.join(" "))
+		let d = r[0]
+		let m = "Location: " + d.location.name + "\n"
+		m += "Temperature: " + d.current.temperature + "\n"
+		m += "Sky: " + d.current.skytext + "\n"
+		m += "Observation time: " + d.current.date + " " + d.current.observationtime
+		api.sendMessage(m, event.threadID, event.messageID)
+	}
+}
