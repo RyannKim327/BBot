60ef4b92562555498c784373e11483f857cb47bf
RySes <rksesgundo123@gmail.com>
2022-04-14 00:01:33 +0800

    Add files via upload

---
+++ script/img.js
@@ -0,0 +1,34 @@
+const google = require("googlethis")
+const fs = require("fs")
+
+async function img(query){
+	let result = await google.image(query, {
+		safe: true
+	})
+	return result
+}
+
+async function revImg(attach){
+	let result = await google.search(attach, {
+		ris: true
+		safe: true
+	})
+	return result
+}
+
+module.exports = async (api, body, event) => {
+	if(event.type == "message_reply"){
+		if(event.messageReply.attachments.length > 0 && event.messageReply.attachments[0].type == "photo"){
+			let r = await revImg(event.messageReply.attachments[0].url)
+			let d = r[0]
+			let m = `Result (Reverse Image Search)\nTitle: ${d.title}\nDescriptio : ${d.description}\nSource: ${d.url}`
+			api.sendMessage(m, event.threadID, event.messageID)
+		}
+	}else{
+		let d = body.split(" ")
+		d.shift()
+		d.shift()
+		let r = await img(d.join(" "))
+		
+	}
+}
