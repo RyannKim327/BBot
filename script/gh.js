f1c81214797e40448114f176b0c2d471e513d1e9
RySes <rksesgundo123@gmail.com>
2022-04-13 18:41:57 +0800

    Add files via upload

--- script/gh.js
+++ script/gh.js
@@ -4,18 +4,18 @@ const request = require("request")
 module.exports = async (api, body, event) => {
 	let data = body(" ")[2]
 	let url = "https://github-readme-stats.vercel.app/api?username=" + data + "&show_icons=true&theme=radical"
-	let file = fs.createWriteStream("gh.jpg")
+	let file = fs.createWriteStream("gh.gif")
 	let r = request(encodeURI(url))
 	r.pipe(file)
 	file.on("close", () => {
 		api.sendMessage({
-			attachment: fs.createReadStream(__dirname + "/../gh.jpg").on("end", async () => {
-				if(fs.existsSync(__dirname + "/../gh.jpg")){
-					fs.unlink(__dirname + "/../gh.jpg", (err) => {
+			attachment: fs.createReadStream(__dirname + "/../gh.gif").on("end", async () => {
+				if(fs.existsSync(__dirname + "/../gh.gif")){
+					fs.unlink(__dirname + "/../gh.gif", (err) => {
 						if(err) return console.error("Error [Github]: " + err)
 					})
 				}
 			})
 		}, event.threadID, messageID)
-	}
+	})
 }
