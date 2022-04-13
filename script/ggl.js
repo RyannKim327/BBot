c97687b19f1d08a70885ec95138e4a32c981d265
RySes <rksesgundo123@gmail.com>
2022-04-14 06:21:06 +0800

    update ggl.js

--- script/ggl.js
+++ script/ggl.js
@@ -57,7 +57,7 @@ module.exports = async (api, body, event) => {
 							})
 						}, event.threadID, event.messageID)
 					})
-				}
+				})
 			}else{
 				api.sendMessage(`Result [Dictionary]:\n${output.word} (${output.phonetic})\n\nDefinitions:\n${definitions}\n\nExamples:\n${examples}`, event,threadID, event.messageID)
 			}
