f1529b31e61abf99baa1e403b0c729518e1752f3
RySes <rksesgundo123@gmail.com>
2022-04-14 06:29:05 +0800

    update qr.js

--- script/qr.js
+++ script/qr.js
@@ -26,23 +26,27 @@ module.exports = (api, body, event) => {
 		let xpl = body.split(" ")
 		xpl.shift()
 		xpl.shift()
-		let data = "http://api.qrserver.com/v1/create-qr-code/?150x150&data=" + xpl.join(" ")
-		let f = fs.createWriteStream("qr.jpg")
-		let res = request(encodeURI(data))
-		res.pipe(f)
-		f.on("close", () => {
-			api.sendMessage({
-				body: "QR Code Generated",
-				attachment: fs.createReadStream(__dirname + "/../qr.jpg").on("end", async () => {
-					if(fs.existsSync(__dirname + "/../qr.jpg")){
-						fs.unlink(__dirname + "/../qr.jpg", (err) => {
-							if(err){
-								console.log(err)
-							}
-						})
-					}
-				})
-			}, event.threadID, event.messageID)
-		})
+		if(xpl.length > 0){
+			let data = "http://api.qrserver.com/v1/create-qr-code/?150x150&data=" + xpl.join(" ")
+			let f = fs.createWriteStream("qr.jpg")
+			let res = request(encodeURI(data))
+			res.pipe(f)
+			f.on("close", () => {
+				api.sendMessage({
+					body: "QR Code Generated",
+					attachment: fs.createReadStream(__dirname + "/../qr.jpg").on("end", async () => {
+						if(fs.existsSync(__dirname + "/../qr.jpg")){
+							fs.unlink(__dirname + "/../qr.jpg", (err) => {
+								if(err){
+									console.log(err)
+								}
+							})
+						}
+					})
+				}, event.threadID, event.messageID)
+			})
+		}else{
+			api.sendMessage(`QR Code Command:\nThis command contains of 2 features, the first one is by executing the command in message reply, and the other one is by using this format in normal message:\nNoBhie: qr <query>.`, event.threadID, event.messageID)
+		}
 	}
 }
