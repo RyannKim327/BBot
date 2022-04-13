ffaf14331397655e0a8f536f1fe3f6d8cb02631d
RySes <rksesgundo123@gmail.com>
2022-04-14 05:56:56 +0800

    update ggl.js

--- script/ggl.js
+++ script/ggl.js
@@ -18,51 +18,57 @@ module.exports = async (api, body, event) => {
 	let data = body.split(" ")
 	data.shift()
 	data.shift()
-	let res = await search(data.join(" "))
-	if(res.knowledge_panel.title != "N/A" && res.knowledge_panel.description != "N/A"){
-		let output = res.knowledge_panel
-		console.log("Log [Info]: " + output.title)
-		api.sendMessage(`Result [Information]:\n${output.title}\n${output.description}`, event.threadID, event.messageID)
-	}else if(res.translation != undefined){
-		let output = res.translation
-		api.sendMessage(`Result [Translate]:\nTranslation from ${output.source_language} to ${output.target_language}\nOriginal: ${output.source_text}\nTranslated: ${output.target_text}`, event.threadID, event.messageID)
-	}else if(res.dictionary != undefined){
-		let output = res.dictionary
-		let definitions = ""
-		let examples = ""
-		let defines = output.definitions
-		for(let i = 0; i < defines.length; i++){
-		  definitions += (i + 1) + ": " + defines[i] + "\n"
-		}
-		if(output.examples != undefined){
-			let ex = output.examples
-			for(let i = 0; i < ex.length; i++){
-		 	 examples += (i + 1) + ": " + ex[i] + "\n"
+	if(data.length > 0){
+		let res = await search(data.join(" "))
+		if(res.knowledge_panel.title != "N/A" && res.knowledge_panel.description != "N/A"){
+			let output = res.knowledge_panel
+			console.log("Log [Info]: " + output.title)
+			api.sendMessage(`Result [Information]:\n${output.title}\n${output.description}`, event.threadID, event.messageID)
+		}else if(res.translation != undefined){
+			let output = res.translation
+			api.sendMessage(`Result [Translate]:\nTranslation from ${output.source_language} to ${output.target_language}\nOriginal: ${output.source_text}\nTranslated: ${output.target_text}`, event.threadID, event.messageID)
+		}else if(res.dictionary != undefined){
+			let output = res.dictionary
+			let definitions = ""
+			let examples = ""
+			let defines = output.definitions
+			for(let i = 0; i < defines.length; i++){
+			  definitions += (i + 1) + ": " + defines[i] + "\n"
 			}
-		}
-		if(output.audio != null){
-			let file = fs.createWriteStream(`${output.word}.mp3`)
-			http.get(output.audio, (p) => {
-				p.pipe(file)
-				file.on("finish", () => {
-					api.sendMessage({
-						body: `Result [Dictionary]:\n${output.word} (${output.phonetic})\n\nDefinitions:\n${definitions}\n\nExamples:\n${examples}`,
-						attachment: fs.createReadStream(`${__dirname}/../${output.word}.mp3`).on("end", async () => {
-							if(fs.existsSync(__dirname + "/../" + output.word + ".mp3")){
-								fs.unlink(`${__dirname}/../${output.word}.mp3`, (err) => {
-									if(err) return console.error("Error [Dictionary audio]: " + err)
-								})
-							}
-						})
-					}, event.threadID, event.messageID)
+			if(output.examples != undefined){
+				let ex = output.examples
+				for(let i = 0; i < ex.length; i++){
+				 examples += (i + 1) + ": " + ex[i] + "\n"
+				}
+			}
+			if(output.audio != null){
+				let file = fs.createWriteStream(`${output.word}.mp3`)
+				http.get(output.audio, (p) => {
+					p.pipe(file)
+					file.on("finish", () => {
+						api.sendMessage({
+							body: `Result [Dictionary]:\n${output.word} (${output.phonetic})\n\nDefinitions:\n${definitions}\n\nExamples:\n${examples}`,
+							attachment: fs.createReadStream(`${__dirname}/../${output.word}.mp3`).on("end", async () => {
+								if(fs.existsSync(__dirname + "/../" + output.word + ".mp3")){
+									fs.unlink(`${__dirname}/../${output.word}.mp3`, (err) => {
+										if(err) return console.error("Error [Dictionary audio]: " + err)
+									})
+								}
+							})
+						}, event.threadID, event.messageID)
+					})
 				})
-			})
+			}else{
+				api.sendMessage(`Result [Dictionary]:\n${output.word} (${output.phonetic})\n\nDefinitions:\n${definitions}\n\nExamples:\n${examples}`, event,threadID, event.messageID)
+			}
 		}else{
-			api.sendMessage(`Result [Dictionary]:\n${output.word} (${output.phonetic})\n\nDefinitions:\n${definitions}\n\nExamples:\n${examples}`, event,threadID, event.messageID)
+			let output = res.results[0]
+			api.sendMessage(`Result [Results]:\n${output.title}\n~${output.description}\nSource: ${output.url}`, event.threadID, event.messageID)
 		}
 	}else{
-		let output = res.results[0]
-		api.sendMessage(`Result [Results]:\n${output.title}\n~${output.description}\nSource: ${output.url}`, event.threadID, event.messageID)
+		api.sendMessage(`Google Command:
+		Test
+		`, event.threadID, event.messageID)
 	}
 }
 
