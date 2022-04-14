882ea0fdc340a57b9a415efda0a22d3ff4ff5994
RySes <rksesgundo123@gmail.com>
2022-04-14 21:23:05 +0800

    update chat.js

--- script/chat.js
+++ script/chat.js
@@ -1,13 +1,13 @@
 const chatbot = require("djs-chatbot")
 
 async function bot(message){
-	let response = client.on("message", async msg => {
+	client.on("message", async msg => {
 		const reply = await chatbot.chat({
 			Message: message
 		})
 		return msg.channel.send(reply)
 	})
-	return response
+	return client
 }
 
 module.exports = async (api, body, event) => {
