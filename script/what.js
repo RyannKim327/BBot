cf6bcf421173e66b01ca7cb2ffe1471b3944a3c8
RySes <rksesgundo123@gmail.com>
2022-04-14 16:29:02 +0800

    update what.js

--- script/what.js
+++ script/what.js
@@ -16,77 +16,81 @@ module.exports = (api, body, event) => {
 	w.shift()
 	w.shift()
 	w.shift()
-	let o = whatIs(w.join(" "))
-	let r = ""
-	o.then((response) => {
-		r = "You've searched about the word \"" + response.word + "\"\n"
-		if(response.phonetics[0].text != undefined){
-			let p = response.phonetics
-			r += p[0].text + "\n"
-		}
-		if(response.origin != undefined){
-			r += response.origin + "\n"
-		}
-		if(response.meanings != undefined){
-			let means = response.meanings
-			for(let i = 0; i < means.length; i++){
-				r += "Part of speech: " + means[i].partOfSpeech + "\n"
-				for(let j = 0; j < means[i].definitions.length; j++){
-					let d = means[i].definitions[j]
-					if(d.definition != undefined){
-						r += "[" + (j + 1) + "] " + d.definition + "\n"
-						if(d.example != undefined){
-							r += "Example " + d.example + "\n\n"
-						}else{
-							r += "\n\n"
+	if(w.length > 0){
+		let o = whatIs(w.join(" "))
+		let r = ""
+		o.then((response) => {
+			r = "You've searched about the word \"" + response.word + "\"\n"
+			if(response.phonetics[0].text != undefined){
+				let p = response.phonetics
+				r += p[0].text + "\n"
+			}
+			if(response.origin != undefined){
+				r += response.origin + "\n"
+			}
+			if(response.meanings != undefined){
+				let means = response.meanings
+				for(let i = 0; i < means.length; i++){
+					r += "Part of speech: " + means[i].partOfSpeech + "\n"
+					for(let j = 0; j < means[i].definitions.length; j++){
+						let d = means[i].definitions[j]
+						if(d.definition != undefined){
+							r += "[" + (j + 1) + "] " + d.definition + "\n"
+							if(d.example != undefined){
+								r += "Example " + d.example + "\n\n"
+							}else{
+								r += "\n\n"
+							}
 						}
 					}
 				}
 			}
-		}
-		if(response.sourceUrls != undefined){
-			r += "References:\n"
-			let sauce = response.sourceUrls
-			for(let i = 0; i < sauce.length; i++){
-				r += sauce[i] + "\n"
-			}
-		}
-		if(response.phonetics != undefined){
-			let p = response.phonetics
-			for(let i = 0; i < p.length; i++){
-				if(p[i].audio.includes("https://")){
-					let f = fs.createWriteStream("whatis.mp3")
-					let g = http.get(p[i].audio, (rs) => {
-						rs.pipe(f)
-						f.on("finish", (err) => {
-							api.sendMessage({
-								body: r,
-								attachment: fs.createReadStream(__dirname + "/../whatis.mp3").on("end", async () => {
-									if(fs.existsSync(__dirname + "/../whatis.mp3")){
-										fs.unlink(__dirname + "/../whatis.mp3", (err) => {
-											if(err){
-												console.log(err)
-											}else{
-												console.log("Done")
-											}
-										})
-									}
-								})
-							}, event.threadID, event.messageID)
-						})
-					})
-					break
-				}else{
-					if(i >= p.length){
-						api.sendMessage(r, event.threadID, event.messageID)
-					}
+			if(response.sourceUrls != undefined){
+				r += "References:\n"
+				let sauce = response.sourceUrls
+				for(let i = 0; i < sauce.length; i++){
+					r += sauce[i] + "\n"
 				}
 			}
-		}else{
-			api.sendMessage(r, event.threadID, event.messageID)
-		}
-	}).catch((err) => {
-		api.sendMessage("Word is not found", event.threadID, event.messageID)
-		console.log("Error " + err)
-	})
+			if(response.phonetics != undefined){
+				let p = response.phonetics
+				for(let i = 0; i < p.length; i++){
+					if(p[i].audio.includes("https://")){
+						let f = fs.createWriteStream("whatis.mp3")
+						let g = http.get(p[i].audio, (rs) => {
+							rs.pipe(f)
+							f.on("finish", (err) => {
+								api.sendMessage({
+									body: r,
+									attachment: fs.createReadStream(__dirname + "/../whatis.mp3").on("end", async () => {
+										if(fs.existsSync(__dirname + "/../whatis.mp3")){
+											fs.unlink(__dirname + "/../whatis.mp3", (err) => {
+												if(err){
+													console.log(err)
+												}else{
+													console.log("Done")
+												}
+											})
+										}
+									})
+								}, event.threadID, event.messageID)
+							})
+						})
+						break
+					}else{
+						if(i >= p.length){
+							api.sendMessage(r, event.threadID, event.messageID)
+						}
+					}
+				}
+			}else{
+				api.sendMessage(r, event.threadID, event.messageID)
+			}
+		}).catch((err) => {
+			api.sendMessage("Word is not found", event.threadID, event.messageID)
+			console.log("Error " + err)
+		})
+	}else{
+		api.sendMessage(`Dictionary Command: This command must have the word you want to know. Try to follow this format:\nNoBhie: what is <word>`, event.threadID, event.messageID)
+	}
 }
