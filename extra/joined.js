aeb16bcfc5bd6af1490de0f962a2a4a209d82693
RySes <rksesgundo123@gmail.com>
2022-04-16 12:00:08 +0800

    update joined.js

--- extra/joined.js
+++ extra/joined.js
@@ -1,13 +1,13 @@
 const fs = require("fs")
 
-module.exports = (api, event) => {
+module.exports = async (api, event) => {
 	console.log("Test")
 	if(event.type == "event"){
 		console.log("Working event")
 		switch(event.logMessageType){
 			case "log:subscribe":
 				console.log("Log [Subs]")
-				api.getThreadInfo(event.threadID, async (err, data) => {
+				/*api.getThreadInfo(event.threadID, async (err, data) => {
 					if(err) return console.error("Error [Log Subscribe]: " + err)
 					if(data.isGroup){
 						const joiner = await event.logMessageData.addedParticipants
@@ -44,11 +44,45 @@ module.exports = (api, event) => {
 						}
 						api.sendMessage(messages, event.threadID)
 					}
-				})
+				})*/
+				let thread = await api.getThreadInfo(event.threadID)
+				if(thread.isGroup){
+					const joiner = await event.logMessageData.addedParticipants
+					const me = api.getCurrentUserID()
+					let messages = {
+						body: "",
+						mentions: []
+					}
+					for(let newb of joiner){
+						if(newb == me){
+							api.sendMessage(fs.readFileSync("txt/abt.txt", "utf8"), event.threadID)
+						}else{
+							const id = newb.userFbId
+							let user = await api.getUserInfo(id)[id]
+							let g = ""
+							switch(user.gender){
+								case 1:
+									g = "Ms."
+								break
+								case 2:
+									g = "Mr."
+								break
+								default:
+									g = "Mr./Ms."
+							}
+							messages.body = `Welcome to ${thread.threadName}, ${g} ${user.name}. Enjoy your staying here, always be patience and be active if you can. Respect all members specially admins.`
+							messages.mentions.push = [{
+								id,
+								tag: `${user.name}`
+							}]
+							api.sendMessage(messages, event.threadID)
+						}
+					}
+				}
 			break
 			case "log:unsubscribe":
 				console.log("Exit")
 			break
 		}
-	}
+	}5
 }
