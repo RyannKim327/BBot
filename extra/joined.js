cb42ebf8ac38c5b25b6b0dbace5c797b13d7658b
RyannKim327 <rksesgundo123@gmail.com>
2022-04-16 13:28:18 +0800

    removed comment

--- extra/joined.js
+++ extra/joined.js
@@ -7,44 +7,6 @@ module.exports = async (api, event) => {
 		switch(event.logMessageType){
 			case "log:subscribe":
 				console.log("Log [Subs]")
-				/*api.getThreadInfo(event.threadID, async (err, data) => {
-					if(err) return console.error("Error [Log Subscribe]: " + err)
-					if(data.isGroup){
-						const joiner = await event.logMessageData.addedParticipants
-						const me = api.getCurrentUserID()
-						let messages = {
-							body: "",
-							mentions: []
-						}
-						for(let newbies of joiner){
-							if(newbies == me){
-								api.sendMessage(fs.readFileSync("txt/abt.txt", "utf-8"), event.threadID)
-								break
-							}
-							const id = newbies.userFbId
-							api.getUserInfo(id, (err, user_data) => {
-								if(err) return console.err("Error [User joiner]: " + err)
-								let g = ""
-								let user = user_data[id]
-								switch(user.gender){
-									case 0:
-										g = "Ms."
-									break
-									case 1:
-										g = "Mr."
-									break
-									default:
-										g = "Mr./Ms."
-								}
-								messages.body = `Welcome to ${data.threadName}, ${g} ${user.name}. Please follow rules and be active. Communicate with others and respect all members speciay the admins.`
-								messages.mentions.push[{
-									id, tag: `${user.name}`
-								}]
-							})
-						}
-						api.sendMessage(messages, event.threadID)
-					}
-				})*/
 				let thread = await api.getThreadInfo(event.threadID)
 				if(thread.isGroup){
 					const joiner = await event.logMessageData.addedParticipants
