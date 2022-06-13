c6e71fb68f424ca91141a3726e299c5c1c24fb29
RyannKim327 <rksesgundo123@gmail.com>
2022-06-13 14:17:48 +0800

    06-13-22 14:17

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const adminPostfix = " >>"
 
 const bad_regex = /JC, ([\w]+) is a bad word./
 
-//const gc = process.env['gc']
+const gc = process.env['gc']
 let vip = []
 let gc_admin = []
 
@@ -316,7 +316,7 @@ login({appState: JSON.parse(process.env['state'])}, (err, api) => {
 					if (err) return console.error("Error [Auto send thread]: " + err)
 					api.sendMessage(`A message sent to: ${data.threadName}\nMessage: ${body}`, say_thread)
 				})
-			}else if((json.status && body.startsWith(prefix) && !json.ban.includes(senderID) && !json.off.includes(threadID)) /* || gc.includes(threadID) */ || json.test.includes(senderID) || vip.includes(senderID)){
+			}else if((json.status && body.startsWith(prefix) && !json.ban.includes(senderID) && !json.off.includes(threadID)) || gc.includes(threadID) || json.test.includes(senderID) || vip.includes(senderID)){
 				if(filter(low_body) && !vip.includes(senderID)){
 					api.setMessageReaction("🥲", messageID, (err) => {}, true)
 				}else{
