d7cb552d310ef21580df895a05974182d06d4f88
RyannKim327 <rksesgundo123@gmail.com>
2022-06-28 09:21:44 +0800

    06-28-22 09:21

--- configs/commands.js
+++ configs/commands.js
@@ -72,7 +72,7 @@ module.exports = async (api, event, pre, gc, vip) => {
 		let { data } = await axios.get("https://ryannkim327.github.io/Pinoy-Bugtong-api/")
 		let $ = cheerio.load(data)
 		let o = $("p").attr("id")
-		api.sendMessage(data, event.threadID)
+		api.sendMessage(cheerio.text(data), event.threadID)
 		api.sendMessage(o.text(), event.threadID)
 	}
 	else if(regex_game_dice.test(body)){
