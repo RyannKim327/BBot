9874a8d45a0cfbac539a43f55e9532b57703792d
RyannKim327 <rksesgundo123@gmail.com>
2022-06-29 01:05:49 +0800

    06-29-22 01:05

--- script/ridles.js
+++ script/ridles.js
@@ -2,14 +2,16 @@ const axios = require("axios")
 
 async function rids(){
 	let result = await axios.get("https://api-pinoy-bugtong.vercel.app").then((r) => {
+		console.log(r)
 		return r.data
 	}).catch((e) => {
+		console.error("Error [api]: " + e)
 		return null
 	})
 	return result
 }
 
-module.exports = (api, event, regex) => {
-	let riddle = rids()
+module.exports = async (api, event, regex) => {
+	let riddle = await rids()
 	api.sendMessage(riddle.b, event.threadID)
 }
