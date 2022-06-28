7ed29e1f606495219f1f4b542762fe40b0a6b211
RyannKim327 <rksesgundo123@gmail.com>
2022-06-28 09:19:45 +0800

    06-28-22 09:19

--- configs/commands.js
+++ configs/commands.js
@@ -71,7 +71,7 @@ module.exports = async (api, event, pre, gc, vip) => {
 	if(body == "JC, api test"){
 		let { data } = await axios.get("https://ryannkim327.github.io/Pinoy-Bugtong-api/")
 		let $ = cheerio.load(data)
-		let o = $("p")
+		let o = $("p").attr("id")
 		api.sendMessage(data, event.threadID)
 		api.sendMessage(o.text(), event.threadID)
 	}
