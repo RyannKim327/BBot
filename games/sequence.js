3ab7090818b2046f56104942d8e6953340352da0
RyannKim327 <rksesgundo123@gmail.com>
2022-06-30 14:00:30 +0800

    06-30-22 14:00

--- games/sequence.js
+++ games/sequence.js
@@ -1,15 +1,15 @@
 const fs = require("fs")
 
 function fibbo(){
-	let a = Math.floor(Math.random() * 100)
+	let a = Math.floor(Math.random() * 100) + 1
 	let p = Math.floor(Math.random() * 14) + 1
 	let r = [1]
 	let t = a
 	let o = ""
 	for(let i = 1; i < 15; i++){
 		let j = r[i - 1] + t
-		t = r[r - 1]
-		r[i] = t
+		t = r[i - 1]
+		r[i] = j
 	}
 	for(let i = 0; i < r.length; i++){
 		o += ((i == p) ? "*" : r[i]) + ", "
